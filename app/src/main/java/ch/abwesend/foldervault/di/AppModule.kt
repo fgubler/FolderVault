1e4cf93c55343aef813d57393467256445942901
Florian Gubler <florian.gubler@ergon.ch>
2026-06-16 16:28:55 +0200

    Fixed deprecation in koin

--- app/src/main/java/ch/abwesend/foldervault/di/AppModule.kt
+++ app/src/main/java/ch/abwesend/foldervault/di/AppModule.kt
@@ -32,7 +32,7 @@ import ch.abwesend.foldervault.view.viewmodel.OnboardingViewModel
 import ch.abwesend.foldervault.view.viewmodel.RestoreViewModel
 import ch.abwesend.foldervault.view.viewmodel.SettingsViewModel
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {

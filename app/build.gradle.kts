225fd5bf9ddd52c7142a91e781c703c23f1330c1
Florian Gubler <florian.gubler@gmail.com>
2026-09-11 15:46:13 +0200

    New version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     defaultConfig {
         applicationId = "ch.abwesend.foldervault"
         minSdk = 26
-        versionCode = 16
-        versionName = "1.9.0"
+        versionCode = 17
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
